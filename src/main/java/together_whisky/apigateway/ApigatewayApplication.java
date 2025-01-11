bf71b46a56532dc4c0973b880de7dc8384ef044a
wook <mystyle730@gmail.com>
2025-01-11 20:29:33 +0900

    init

---
+++ src/main/java/together_whisky/apigateway/ApigatewayApplication.java
@@ -0,0 +1,13 @@
+package together_whisky.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/together_whisky/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package together_whisky.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
